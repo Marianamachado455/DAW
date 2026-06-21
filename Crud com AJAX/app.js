dac7669cfe35d380a5457e963078cac81fbef21d
Mariana Machado <mariana.25104708360006@faeterj-rio.edu.br>
2026-06-21 11:04:44 -0300

    Refactor AJAX calls for CRUD operations

---
+++ Crud com AJAX/app.js
@@ -0,0 +1,60 @@
+//AJAX
+  unction ajax(method, url, data, callback) {
+  var xhr = new XMLHttpRequest();
+
+  xhr.open(method, url, true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      callback(xhr.responseText);
+    }
+  };
+
+  xhr.send(data);
+}
+
+//Criar itens 
+function createItem() {
+  var name = document.getElementById("name").value;
+
+  ajax("POST", "api/create.php", "name=" + name, function () {
+    loadItems();
+  });
+}
+
+//carregar itens
+function loadItems() {
+  ajax("GET", "api/read.php", null, function (res) {
+    var data = JSON.parse(res);
+    var list = document.getElementById("list");
+
+    list.innerHTML = "";
+
+    for (var i = 0; i < data.length; i++) {
+      list.innerHTML +=
+        "<li>" +
+        data[i].name +
+        " " +
+        "<button onclick='deleteItem(" + data[i].id + ")'>X</button>" +
+        " <button onclick='updateItem(" + data[i].id + ")'>Editar</button>" +
+        "</li>";
+    }
+  });
+}
+
+//Atualizar itens
+function updateItem(id) {
+  var name = prompt("Novo nome:");
+
+  ajax("POST", "api/update.php", "id=" + id + "&name=" + name, function () {
+    loadItems();
+  });
+}
+
+//Deletar itens
+function deleteItem(id) {
+  ajax("POST", "api/delete.php", "id=" + id, function () {
+    loadItems();
+  });
+}
